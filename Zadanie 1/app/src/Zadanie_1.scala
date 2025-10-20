4e70df5dfb37f89be7b30085138774a344051781
Wojciech Kotulski <wkot501@gmail.com>
2025-10-20 23:16:25 +0200

    Zadanie 1 - Docker

--- Zadanie 1/app/src/Zadanie_1.scala
+++ Zadanie 1/app/src/Zadanie_1.scala
@@ -2,6 +2,9 @@ package app
 
 object Zadanie_1 extends cask.MainRoutes{
 
+  override def host: String = "0.0.0.0"
+  override def port: Int = 8080
+
   @cask.get("/")
   def helloWorld() ={
     "Hello World!"
