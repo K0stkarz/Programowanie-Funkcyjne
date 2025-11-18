8e94905b425221ba79ef6234cd1b07afcd242846
Wojciech Kotulski <wkot501@gmail.com>
2025-11-18 17:55:10 +0100

    Zadanie 4 - append

--- Zadanie 4/app/src/Zadanie_4.scala
+++ Zadanie 4/app/src/Zadanie_4.scala
@@ -10,5 +10,17 @@ object Zadanie_1 extends cask.MainRoutes{
     "Hello World!"
   }
 
+  @cask.postJson("/append")
+  def append(e: ujson.Value, index: Option[Int] = None, list: Option[Seq[ujson.Value]] = None) = {
+    val new_list = list.getOrElse(Seq.empty)
+    val new_index = index.getOrElse(new_list.length)
+    
+    var (split1, split2) = new_list.splitAt(new_index)
+    var out = split1 ++ List(e) ++ split2
+    ujson.Obj(
+      "list" -> out
+    )
+  }
+
   initialize()
 }
