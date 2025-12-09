73c90baa2f4a6ca4b49687bd475e399560900e4c
Wojciech Kotulski <wkot501@gmail.com>
2025-12-09 19:14:14 +0100

    Zadanie 7 - getRandom_0_to_1

--- Zadanie 7/app/Main.hs
+++ Zadanie 7/app/Main.hs
@@ -7,15 +7,23 @@ import Data.IORef
 
 type Rand a = State Int a
 
+mConstant :: Int
+mConstant = 2147483648  -- 2^31
+
 nextRand :: Rand Int
 nextRand = do
   seed <- get
   let a   = 1103515245
       c   = 12345
-      m   = 2147483648      -- 2^31
+      m   = mConstant
       seed' = (a * seed + c) `mod` m
   put seed'
   return seed'
+
+nextRandDouble :: Rand Double
+nextRandDouble = do
+  r <- nextRand
+  return $ fromIntegral r / fromIntegral mConstant
 
 runWithState :: IORef Int -> Rand a -> IO a
 runWithState ref action = do
@@ -35,4 +43,8 @@ main = do
 
         Scotty.get "/get-random-0-to-maxInt" $ do 
             xs <- liftIO $ runWithState globalSeed nextRand
+            Scotty.json $ object ["result" .= xs]
+
+        Scotty.get "/get-random-0-to-1" $ do 
+            xs <- liftIO $ runWithState globalSeed nextRandDouble
             Scotty.json $ object ["result" .= xs]
